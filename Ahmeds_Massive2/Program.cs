8982619523f6569319e1c170a1d4a83e2140fe5f
PROGRAMIST228 <49342911+PROGRAMIST228@users.noreply.github.com>
2019-04-06 17:39:11 +0300

    Initial commit

---
+++ Ahmeds_Massive2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ahmeds_Massive2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 1;
+            int[] a = new int[100];
+            Random rand = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                a[i] = rand.Next(1, 1000);
+                if (a[i] > max)
+                {
+                    max = a[i];
+                }
+            }
+            Console.WriteLine("" + max);
+            Console.ReadKey();
+        }
+    }
+}
